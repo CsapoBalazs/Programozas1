e847490fe4b735675de8b013a25719839bb604a4
CsapoBalazs <144666719+CsapoBalazs@users.noreply.github.com>
2023-11-06 11:45:23 +0100

    Create n1.py

---
+++ n1.py
@@ -0,0 +1,20 @@
+x = print("Hello")
+print(x)
+
+def fv(num):
+    if num>5:
+        return 0
+    else:
+        print("kisebb")
+    #return 0
+
+y = fv(3)
+print(y)
+
+print("Hello \nWo\trld")
+print(len("\n"))
+print("wshdrzj\\number")
+print("elsosszo", "masodikszo", end=" ")
+print("Python")
+
+print("elsosszo", "masodikszo" , sep = ":")
